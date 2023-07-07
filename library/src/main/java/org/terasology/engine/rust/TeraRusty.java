97c2de739cb91e166aa1151f018954e8447c38d5
Michael Pollind <mpollind@gmail.com>
2023-07-06 19:21:59 -0700

    add win32 surface
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- library/src/main/java/org/terasology/engine/rust/TeraRusty.java
+++ library/src/main/java/org/terasology/engine/rust/TeraRusty.java
@@ -9,10 +9,12 @@ public class TeraRusty {
     private static final NativeLoader loader = new NativeLoader.Builder()
         .build();
 
-    static {
+    public static void Initialize() {
         loader.loadLibrary("tera-rusty", false);
     }
 
     // window init code
     public static native void initializeWindowX11(long displayptr, long windowptr);
+    public static native void initializeWindowWin32(long displayptr, long windowptr);
+
 }
--- rust-library/src/win_init.rs
+++ rust-library/src/win_init.rs
@@ -1,5 +1,5 @@
 use jni::{sys::{ JNIEnv, jlong}, objects::JClass};
-use raw_window_handle::{XlibDisplayHandle, XlibWindowHandle, HasRawWindowHandle, RawWindowHandle,  HasRawDisplayHandle, RawDisplayHandle};
+use raw_window_handle::{XlibDisplayHandle, XlibWindowHandle, HasRawWindowHandle, RawWindowHandle,  HasRawDisplayHandle, RawDisplayHandle, Win32WindowHandle, WindowsDisplayHandle};
 use std::ffi::c_void;
 
 use crate::context::CONTEXT;
@@ -21,14 +21,53 @@ unsafe impl HasRawDisplayHandle for XWindow {
     }
 }
 
+
+
 #[no_mangle]
-pub extern "system" fn Java_org_terasology_engine_core_initializeWindowX11(_jni: JNIEnv, _class: JClass, display_ptr: jlong, window_ptr: jlong)  {
+pub extern "system" fn Java_org_terasology_engine_rust_TeraRusty_initializeWindowX11(_jni: JNIEnv, _class: JClass, display_ptr: jlong, window_ptr: jlong)  {
     let mut win = XWindow {
         window: XlibWindowHandle::empty(),
         display: XlibDisplayHandle::empty()
     };
     win.window.window = window_ptr as u64;
     win.display.display = display_ptr as *mut c_void;
+  
+    let instance = wgpu::Instance::default();
+    let surface_result = unsafe {instance.create_surface(&win) };
+    let surface = match surface_result {
+        Ok(surface) => surface,
+        Err(err) => panic!("problem creating surface: {:?}", err),    
+    };
+    let mut context = CONTEXT.write().unwrap(); 
+    context.wgpu_instance = Some(instance); 
+    context.wgpu_surface = Some(surface);
+}
+
+struct Win32Window {
+    window: Win32WindowHandle,
+    display: WindowsDisplayHandle
+}
+
+
+unsafe impl HasRawWindowHandle for Win32Window {
+    fn raw_window_handle(&self) -> raw_window_handle::RawWindowHandle {
+        return RawWindowHandle::Win32(self.window);
+    }
+}
+
+unsafe impl HasRawDisplayHandle for Win32Window {
+    fn raw_display_handle(&self) -> raw_window_handle::RawDisplayHandle {
+        return RawDisplayHandle::Windows(self.display);
+    }
+}
+
+#[no_mangle]
+pub extern "system" fn Java_org_terasology_engine_rust_TeraRusty_initializeWindowWin32(_jni: JNIEnv, _class: JClass, window_ptr: jlong)  {
+    let mut win = Win32Window {
+        window: Win32WindowHandle::empty(),
+        display: WindowsDisplayHandle::empty()
+    };
+    win.window.hwnd = window_ptr as *mut c_void;
   
     let instance = wgpu::Instance::default();
     let surface_result = unsafe {instance.create_surface(&win) };
