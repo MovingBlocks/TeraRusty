db1857c571c3ed79e7da201917df6b1340a740ae
Michael Pollind <mpollind@gmail.com>
2023-07-08 20:36:13 -0700

    feat: add working TeraRusty
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- core/src/main/java/org/terasology/engine/rust/TeraRusty.java
+++ core/src/main/java/org/terasology/engine/rust/TeraRusty.java
@@ -4,15 +4,15 @@
 
 package org.terasology.engine.rust;
 
-import fr.stardustenterprises.yanl.NativeLoader;
+//import fr.stardustenterprises.yanl.NativeLoader;
 
 public class TeraRusty {
-    private static final NativeLoader loader = new NativeLoader.Builder()
-        .build();
+//    private static final NativeLoader loader = new NativeLoader.Builder()
+//        .build();
 
-    public static void initialize() {
-        loader.loadLibrary("tera-rusty", false);
-    }
+//    public static void initialize() {
+//        loader.loadLibrary("tera-rusty", false);
+//    }
 
     private TeraRusty() {
 
